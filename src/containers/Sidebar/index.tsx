17406e63f30d9d1dd2767b291d7131f5acc0fcd2
Gustavo GDF <gustavoaguiar398@gmail.com>
2024-11-21 15:37:02 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/GDFgustavo.png" />
+    <Titulo fontSize={20}>Gustavo Aguiar</Titulo>
+  </aside>
+)
+
+export default Sidebar
