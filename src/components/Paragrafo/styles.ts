0d08a32a6f91cedce8ed7e4dad754335a796152a
Gustavo GDF <gustavoaguiar398@gmail.com>
2024-11-21 16:19:39 -0300

    criando a seção de projetos e o componente de projeto

--- src/components/Paragrafo/styles.ts
+++ src/components/Paragrafo/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { Props } from '.'
 
 export const P = styled.p<Props>`
-  fontsize: 14px;
+  font-size: 14px;
   color: ${(props) => (props.tipo === 'principal' ? '#282a35' : '#949494')};
   line-height: 22px;
 `
